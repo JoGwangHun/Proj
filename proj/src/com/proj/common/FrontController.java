f8873af870b4f92ed4d9be436ddb0560bd5874ca
Academy <jkl3620@naver.com>
2021-06-02 14:18:50 +0900

    123

--- proj/src/com/proj/common/FrontController.java
+++ proj/src/com/proj/common/FrontController.java
@@ -64,18 +64,6 @@ public class FrontController  extends HttpServlet {
 		map.put("/infoUpdatePwdForm.do", new infoUpdatePwdForm());
 		map.put("/memberInfo.do", new memberInfo());
 
-
-		map.put("/memberDelete.do", new memberDelete());
-		map.put("/memberDeleteForm.do", new memberDeleteForm());
-		
-
-		map.put("/thingDelete.do", new ThingDelete());
-		map.put("/thingDelete.do", new ThingDelete());
-		map.put("/thingUpdate.do", new ThingUpdate());
-		map.put("/thingForm.do", new ThingForm());
-
-
-
 		// 장바구니 카운트
 		map.put("/addCnt.do", new AddCartCnt());
 	}
